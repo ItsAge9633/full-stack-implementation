758252f6a73c0f1248a2c7f5f942c1b7a2c6b27d
Umer Shaikh <100994010+ItsAge9633@users.noreply.github.com>
2023-05-25 22:33:30 +0530

    navbar

---
+++ imports/nav-stud.php
@@ -0,0 +1,75 @@
+  <!-- ======= Header ======= -->
+  <header id="header" class="header fixed-top d-flex align-items-center">
+
+    <div class="d-flex align-items-center justify-content-between">
+      <a href="index.php" class="logo d-flex align-items-center">
+        <img src="../assets/img/logo.png" alt="">
+        <span class="d-none d-lg-block">Rich Tech</span>
+      </a>
+      <i class="bi bi-list toggle-sidebar-btn"></i>
+    </div><!-- End Logo -->
+
+    <div class="search-bar">
+      <form class="search-form d-flex align-items-center" method="POST" action="#">
+        <input type="text" name="query" placeholder="Search" title="Enter search keyword">
+        <button type="submit" title="Search"><i class="bi bi-search"></i></button>
+      </form>
+    </div><!-- End Search Bar -->
+
+    <nav class="header-nav ms-auto">
+      <ul class="d-flex align-items-center">
+
+        <li class="nav-item d-block d-lg-none">
+          <a class="nav-link nav-icon search-bar-toggle " href="#">
+            <i class="bi bi-search"></i>
+          </a>
+        </li><!-- End Search Icon-->
+
+      </ul>
+    </nav><!-- End Icons Navigation -->
+
+  </header><!-- End Header -->
+
+  <!-- ======= Sidebar ======= -->
+  <aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link " href="../index.php">
+          <i class="bi bi-people"></i>
+          <span>Admin Login</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link " href="squizregister.php">
+        <i class="bi bi-grid"></i>
+        <span>Enter Quiz</span>
+        </a>
+    </li><!-- End FInance Nav -->
+      
+      <li class="nav-item">
+        <a class="nav-link " href="result.php">
+          <i class="bi bi-grid"></i>
+          <span>Results</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link " href="#">
+          <i class="bi bi-people"></i>
+          <span>About Us</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link " href="#">
+          <i class="bi bi-people"></i>
+          <span>Contact Us</span>
+        </a>
+      </li>
+
+  </aside><!-- End Sidebar-->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
